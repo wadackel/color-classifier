31d91540ec312c8f49ce83979520b99a20d674c9
tsuyoshiwada <v23.b.wada@gmail.com>
2016-05-07 22:29:13 +0900

    Add test files

---
+++ test/color-classify.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const assert = require("power-assert");
+
+
+describe("ColorClassify", () => {
+
+  describe("__TODO__", () => {
+    it("__todo__", () => {
+      assert(1 === 2);
+    });
+  });
+});
