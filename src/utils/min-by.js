15797b15d518abb0d7ff8a3faacfba516115a7bd
tsuyoshiwada <v23.b.wada@gmail.com>
2016-05-11 19:43:07 +0900

    Implement min-by

--- src/utils/min-by.js
+++ src/utils/min-by.js
@@ -1,2 +1,11 @@
-export default function minBy(array, prop) {
+export default function minBy(array, key) {
+  const sortedArray = [...array];
+
+  sortedArray.sort((a, b) => {
+    if (a[key] < b[key]) return -1;
+    if (a[key] > b[key]) return 1;
+    return 0;
+  });
+
+  return sortedArray.shift();
 }
