fa2945fe8cf4c2176ea7d4014c15199b12ecd005
tsuyoshiwada <v23.b.wada@gmail.com>
2016-05-15 17:36:55 +0900

    Add classify tests

--- test/color-classify.js
+++ test/color-classify.js
@@ -52,12 +52,34 @@ describe("ColorClassify", () => {
       assert(isPlainObject(c.classify("#afe713")) === true);
       assert(isPlainObject(c.classify("#139ce7")) === true);
 
+      assert(isPlainObject(c.classify("#fff", "hsv"))    === true);
+      assert(isPlainObject(c.classify("#888", "hsv"))    === true);
+      assert(isPlainObject(c.classify("#5c3cd4", "hsv")) === true);
+      assert(isPlainObject(c.classify("#7f437a", "hsv")) === true);
+      assert(isPlainObject(c.classify("#afe713", "hsv")) === true);
+      assert(isPlainObject(c.classify("#139ce7", "hsv")) === true);
+
       assert(typeof c.classify("#fff", "hex")    === "string");
       assert(typeof c.classify("#888", "hex")    === "string");
       assert(typeof c.classify("#5c3cd4", "hex") === "string");
       assert(typeof c.classify("#7f437a", "hex") === "string");
       assert(typeof c.classify("#afe713", "hex") === "string");
       assert(typeof c.classify("#139ce7", "hex") === "string");
+    });
+
+    it("should be classify color", () => {
+      const c = new ColorClassifier(["#ffffff", "#000000"]);
+
+      assert(c.classify("#fafafa", "hex") === "#ffffff");
+      assert(c.classify("#121212", "hex") === "#000000");
+
+      c.setAlgorithmType(AlgorithmTypes.HSV);
+      assert(c.classify("#fafafa"), "hex" === "#ffffff");
+      assert(c.classify("#121212"), "hex" === "#ffffff");
+
+      c.setAlgorithmType(AlgorithmTypes.RGB);
+      assert(c.classify("#fafafa"), "hex" === "#ffffff");
+      assert(c.classify("#121212"), "hex" === "#ffffff");
     });
   });
 
@@ -69,6 +91,45 @@ describe("ColorClassify", () => {
       assert(Array.isArray(c.classifyFromArray(["#140dc7"])) === true);
       assert(Array.isArray(c.classifyFromArray(["#000", "#ff0"])) === true);
       assert(Array.isArray(c.classifyFromArray(["#000000", "#808080", "#c0c0c0", "#ffffff", "#0000ff", "#000080", "#008080", "#008000", "#00ff00", "#00ffff", "#ffff00", "#ff0000", "#ff00ff", "#808000", "#800080", "#800000"])) === true);
+    });
+
+    it("should be classify colors", () => {
+      const c = new ColorClassifier(["#ffffff", "#000000"]);
+
+      assert.deepStrictEqual(c.classifyFromArray(["#fafafa", "#fefefe", "#121212", "#333333"], "hex"), [
+        {
+          palette: "#ffffff",
+          colors: ["#fafafa", "#fefefe"]
+        },
+        {
+          palette: "#000000",
+          colors: ["#121212", "#333333"]
+        }
+      ]);
+
+      c.setAlgorithmType(AlgorithmTypes.HSV);
+      assert.deepStrictEqual(c.classifyFromArray(["#fafafa", "#fefefe", "#121212", "#333333"], "hex"), [
+        {
+          palette: "#ffffff",
+          colors: ["#fafafa", "#fefefe"]
+        },
+        {
+          palette: "#000000",
+          colors: ["#121212", "#333333"]
+        }
+      ]);
+
+      c.setAlgorithmType(AlgorithmTypes.RGB);
+      assert.deepStrictEqual(c.classifyFromArray(["#fafafa", "#fefefe", "#121212", "#333333"], "hex"), [
+        {
+          palette: "#ffffff",
+          colors: ["#fafafa", "#fefefe"]
+        },
+        {
+          palette: "#000000",
+          colors: ["#121212", "#333333"]
+        }
+      ]);
     });
   });
 });
